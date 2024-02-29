b4ebc6b7645d501757392e3530d8de8f6920af3d
Maksim Egorov <m.egorov193@gmail.com>
2024-02-29 15:09:30 +0400

    upd

--- src/test/java/org/wikipedia/tests/WikiLocalTests.java
+++ src/test/java/org/wikipedia/tests/WikiLocalTests.java
@@ -1,15 +1,39 @@
 package org.wikipedia.tests;
 
+import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
 
 @Tag("local")
 @DisplayName("Тестировании мобильного приложения Wikipedia на локальном эмуляторе")
 public class WikiLocalTests extends TestBase {
     @Test
-    @DisplayName("")
+    @DisplayName("Проверка onboarding screen ")
     void onBoardingPagesTest() {
-
+        step("Проверить первую страницу", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        });
+        step("Проверить вторую страницу", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("New ways to explore"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        });
+        step("Проверить третью страницу", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Reading lists with sync"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        });
+        step("Проверить четвертую страницу", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Send anonymous data"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/acceptButton")).click();
+        });
     }
 }
--- src/test/java/org/wikipedia/tests/WikiRemoteTests.java
+++ src/test/java/org/wikipedia/tests/WikiRemoteTests.java
@@ -4,12 +4,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+
 @Tag("remote")
 @DisplayName("Тестировании мобильного приложения Wikipedia на удаленном эмуляторе")
 public class WikiRemoteTests extends TestBase {
     @Test
-    @DisplayName("")
-    void temp() {
+    @DisplayName("Проверить непустой список выдачи при запросе")
+    void successfulSearchTest() {
+        step("Сделать поисковой запрос", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Porsche");
+        });
 
+        step("Проверить результат запроса", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
     }
 }
